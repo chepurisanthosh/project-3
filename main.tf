a5595809f0704e7ab604ef79e6df68c364631544
chepurisanthosh <52729976+chepurisanthosh@users.noreply.github.com>
2023-07-05 19:14:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,12 +6,21 @@ provider "aws" {
   access_key   = "AKIARQ22MT2VZH2CURUD"
   secret_key  = "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"
 }
+#resource : what changes you want to do
 
-# VPC creation
+resource "aws_instance" "myec2" {
+  ami               = "ami-024e6efaf93d85776"
+  instance_type     = "t2.micro"
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "MyVPC"
+    Name = "Terraform1"
   }
+}
+
+resource "aws_eip" "myeip" {
+  domain = "vpc"
+}
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.myec2.id
+  allocation_id = aws_eip.myeip.id
 }
